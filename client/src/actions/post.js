c5fdb92ac35b5e4a20ecb6e47934aeb3c4e0128b
yousuf1318 <61650368+yousuf1318@users.noreply.github.com>
2021-05-17 13:46:57 +0530

    Update post.js

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -15,7 +15,7 @@ import {
 
 export const getPosts = () => async dispatch => {
     try {
-        const res = await axios.get('/api/posts')
+        const res = await axios.get("https://social-app-b.herokuapp.com/api/post")
 
         dispatch({
             type: GET_POSTS,
@@ -35,7 +35,7 @@ export const getPosts = () => async dispatch => {
 
 export const addLike = id => async dispatch => {
     try {
-        const res = await axios.put(`/api/posts/like/${id}`)
+        const res = await axios.put(`https://social-app-b.herokuapp.com/api/posts/like/${id}`)
 
         dispatch({
             type: UPDATE_LIKES,
@@ -54,7 +54,7 @@ export const addLike = id => async dispatch => {
 
 export const removeLike = id => async dispatch => {
     try {
-        const res = await axios.put(`/api/posts/unlike/${id}`)
+        const res = await axios.put(`https://social-app-b.herokuapp.com/api/posts/unlike/${id}`)
 
         dispatch({
             type: UPDATE_LIKES,
@@ -72,7 +72,7 @@ export const removeLike = id => async dispatch => {
 
 export const deletePost = id => async dispatch => {
     try {
-        await axios.delete(`/api/posts/${id}`)
+        await axios.delete(`https://social-app-b.herokuapp.com/api/posts/${id}`)
 
         dispatch({
             type: DELETE_POST,
@@ -97,7 +97,7 @@ export const addPost = FormData => async dispatch => {
         }
     }
     try {
-        const res = await axios.post('/api/posts', FormData, config)
+        const res = await axios.post('https://social-app-b.herokuapp.com/api/posts', FormData, config)
 
         dispatch({
             type: ADD_POST,
@@ -117,7 +117,7 @@ export const addPost = FormData => async dispatch => {
 
 export const getPost = id => async dispatch => {
     try {
-        const res = await axios.get(`/api/posts/${id}`)
+        const res = await axios.get(`https://social-app-b.herokuapp.com/api/posts/${id}`)
 
         dispatch({
             type: GET_POST,
@@ -140,7 +140,7 @@ export const addComment = (postId, FormData) => async dispatch => {
         }
     }
     try {
-        const res = await axios.post(`/api/posts/comment/${postId}`, FormData, config)
+        const res = await axios.post(`https://social-app-b.herokuapp.com/api/posts/comment/${postId}`, FormData, config)
 
         dispatch({
             type: ADD_COMMENT,
@@ -161,7 +161,7 @@ export const addComment = (postId, FormData) => async dispatch => {
 
 export const deleteComment = (postId, commentId) => async dispatch => {
     try {
-        await axios.delete(`/api/posts/comment/${postId}/${commentId}`)
+        await axios.delete(`https://social-app-b.herokuapp.com/api/posts/comment/${postId}/${commentId}`)
 
         dispatch({
             type: REMOVE_COMMENT,
